f1616e5ff237ddc24623af17bd2f02cb3ee2c9b5
Mdmahadi71 <habibalmasud79@gmail.com>
2024-09-17 07:42:18 -0700

    demo

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,23 @@ import Container from './Components/Container'
 function App() {
 
   let time = new Date().toLocaleTimeString();
-  let [newTime ,setnewTime] = useState(time);
+  let [newTime, setnewTime] = useState(time);
 
-  let updateTime=(() => {
+  let updateTime = (() => {
     let time = new Date().toLocaleTimeString();
     setnewTime(time)
   })
 
 
-  setInterval(updateTime,1000)
+  setInterval(updateTime, 1000)
   return (
     <div className=''>
-      <Container>
-        <div className=" w-[400px] h-[200px] mx-auto bg-black text-center leading-[200px]  my-[200px] rounded-[20px]">
-          <h2 className=' font-Time text-[42px] text-[red]'> {newTime} </h2>
+      <Container className={`my-[200px] `}>
+        <div className="">
+          <h2 className=' font-neto text-[28px] text-center my-4 '>Digital Clock</h2>
+        </div>
+        <div className=" w-[400px] h-[150px] mx-auto bg-black text-center leading-[150px]  rounded-[20px]">
+          <h2 className=' font-Time text-[46px] text-[red]'> {newTime} </h2>
         </div>
       </Container>
     </div>
